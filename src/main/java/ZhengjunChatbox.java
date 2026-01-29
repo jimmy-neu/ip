a0e3b9d1c3854f183ebb664ef4f122297128bd75
jimmy-neu <e0969923@u.nus.edu>
2026-01-29 15:45:10 +0800

    Implement Level-0: Rename, Greet, Exit

---
+++ src/main/java/ZhengjunChatbox.java
@@ -0,0 +1,16 @@
+public class ZhengjunChatbox {
+// Use my chosen name
+    public static void main(String[] args) {
+        String botName = "ZhengjunChatbox";
+        String horizontalLine = "____________________________________________________________";
+        // Skeletal greet the user for level 0
+        System.out.println(horizontalLine);
+        System.out.println("Hello! I'm " + botName);
+        System.out.println("What can I do for you?");
+        System.out.println(horizontalLine);
+
+        // Skeletal exit message for level 0
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
+    }
+}
