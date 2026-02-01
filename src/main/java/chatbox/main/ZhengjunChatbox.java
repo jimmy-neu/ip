4737012559b87d0ea733d391a05b8e0c045abf43
jimmy-neu <e0969923@u.nus.edu>
2026-02-01 21:48:11 +0800

    Implement Level-4 Logic: ToDo, Deadline, Event(3 child classes are created)

--- src/main/java/chatbox/main/ZhengjunChatbox.java
+++ src/main/java/chatbox/main/ZhengjunChatbox.java
@@ -4,20 +4,20 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 /**
- * This Code Block further add the ability to mark and unmark the tasks in the list
+ * This further add the ability to support ToDo, Deadline and  Event.
  */
 public class ZhengjunChatbox {
     public static void main(String[] args) {
         String botName = "ZhengjunChatbox";
         String horizontalLine = "____________________________________________________________";
-        System.out.println(horizontalLine + "\nHello! I'm " + botName + "\nYou can enter text for me to store and key in command: List to view them. \nMoreover, you can mark and unmark tasks. Enter bye to exit\n" + horizontalLine);
+        System.out.println(horizontalLine + "\nHello! I'm " + botName + "\nYou can enter text for me to store and key in command: List to view them. \nMoreover, you can mark and unmark tasks. \nThere are three types of input: Todo, Deadline and Event. Enter bye to exit\n" + horizontalLine);
 
         ArrayList<Task> taskList = new ArrayList<>(); // The Arraylist now store Task class
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
-            String[] parts = input.split(" "); // Splits the input
-            String command = parts[0]; // Obtain the command
+            String[] parts = input.split(" ", 2); // Split the command from the rest of the string
+            String command = parts[0];
 
             if (input.equals("bye")) {
                 break;
@@ -29,7 +29,7 @@ public class ZhengjunChatbox {
                 System.out.println(horizontalLine);
 
             } else if (command.equals("mark")) {
-                int index = Integer.parseInt(parts[1]) - 1; // Convert user input to 0-based index
+                int index = Integer.parseInt(parts[1]) - 1;
                 taskList.get(index).markAsDone();
                 System.out.println(horizontalLine + "\nNice! I've marked this task as done:\n  "
                         + taskList.get(index) + "\n" + horizontalLine);
@@ -40,8 +40,33 @@ public class ZhengjunChatbox {
                 System.out.println(horizontalLine + "\nOK, I've marked this task as not done yet:\n  "
                         + taskList.get(index) + "\n" + horizontalLine);
 
+            } else if (command.equals("todo")) {
+                Task newTask = new ToDo(parts[1]);
+                taskList.add(newTask);
+                System.out.println(horizontalLine + "\nGot it. I've added this task:\n  " + newTask
+                        + "\nNow you have " + taskList.size() + " tasks in the list.\n" + horizontalLine);
+
+            } else if (command.equals("deadline")) {
+                // Split the input into smaller parts
+                // For instance, "return book /by Sunday" can be splited into ["return book", "Sunday"]
+                String[] deadlineParts = parts[1].split(" /by ");
+                Task newTask = new Deadline(deadlineParts[0], deadlineParts[1]);
+                taskList.add(newTask);
+                System.out.println(horizontalLine + "\nGot it. I've added this task:\n  " + newTask
+                        + "\nNow you have " + taskList.size() + " tasks in the list.\n" + horizontalLine);
+
+            } else if (command.equals("event")) {
+                String[] eventParts = parts[1].split(" /from ");
+                String[] timeParts = eventParts[1].split(" /to ");
+                Task newTask = new Event(eventParts[0], timeParts[0], timeParts[1]);
+                taskList.add(newTask);
+                System.out.println(horizontalLine + "\nGot it. I've added this task:\n  " + newTask
+                        + "\nNow you have " + taskList.size() + " tasks in the list.\n" + horizontalLine);
+
             } else {
-                taskList.add(new Task(input));
+                // This handles cases where no specific command matches.
+                Task newTask = new Task(input);
+                taskList.add(newTask);
                 System.out.println(horizontalLine + "\nadded: " + input + "\n" + horizontalLine);
             }
         }
