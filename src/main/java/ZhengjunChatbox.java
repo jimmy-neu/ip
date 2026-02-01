adc2f3179ce7b421d97161ed2dcc3e16211b32b8
jimmy-neu <e0969923@u.nus.edu>
2026-02-01 18:15:35 +0800

    Implement Level-1 and add a  chatbot.main package to wrap the ZhengjunChatbox

--- src/main/java/ZhengjunChatbox.java
+++ src/main/java/ZhengjunChatbox.java
@@ -1,16 +1,28 @@
+package chatbot.main;
+import java.util.Scanner;
+
+/**
+ * This Code Block echoes input until "bye" is received.
+ */
 public class ZhengjunChatbox {
-// Use my chosen name
     public static void main(String[] args) {
         String botName = "ZhengjunChatbox";
         String horizontalLine = "____________________________________________________________";
-        // Skeletal greet the user for level 0
-        System.out.println(horizontalLine);
-        System.out.println("Hello! I'm " + botName);
-        System.out.println("What can I do for you?");
-        System.out.println(horizontalLine);
 
-        // Skeletal exit message for level 0
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(horizontalLine);
+        System.out.println(horizontalLine + "\nHello! I'm " + botName + "\nWhat can I do for you?\n" + horizontalLine);
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        // Level-1: Echo loop logic
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+
+            System.out.println(horizontalLine + "\n" + input + "\n" + horizontalLine);
+        }
+        // Exit message
+        System.out.println(horizontalLine + "\nBye. Hope to see you again soon!\n" + horizontalLine);
+        scanner.close();
     }
 }
